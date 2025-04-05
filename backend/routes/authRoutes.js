82d97d4685d27299abdfff809e1fe422bb8ca1e7
Rohith292 <rohk29@gmail.com>
2025-04-05 19:36:56 +0530

    fixadded some changes to frontend code

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -23,5 +23,10 @@ router.post("/upload-image",upload.single("image"), (req,res)=>{
     const imageUrl=`${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
     res.status(200).json({imageUrl});
 });
+// Logout route
+router.post("/logout", (req, res) => {
+    // If you ever use sessions/cookies, you can clear them here
+    res.status(200).json({ message: "Logged out successfully" });
+});
 
 module.exports=router;
--- frontend/expense-tracker-app/src/utils/apiPaths.js
+++ frontend/expense-tracker-app/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL="http://localhost:8000";
+export const BASE_URL=import.meta.env.VITE_API_BASE_URL||"http://localhost:8000";
 
 //utils/apiPaths.js
 export const API_PATHS={
@@ -6,6 +6,8 @@ export const API_PATHS={
         LOGIN:"/api/v1/auth/login",
         REGISTER:"/api/v1/auth/register",
         GET_USER_INFO:"/api/v1/auth/getUser",
+        LOGOUT: "/api/v1/auth/logout",
+
     },
     DASHBOARD:{
         GET_DATA:"/api/v1/dashboard",
