7fde2cad54166b95e9154e17bf15568eb5eefd05
Rohith292 <rohk29@gmail.com>
2025-06-21 23:31:32 +0530

    Fix CORS config for deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app=express();
 app.use(
     cors({
         origin:process.env.CLIENT_URL,
-        credentials:true,
+        credentials:true,//added comments
         methods:["GET","POST","PUT","DELETE"],
         allowedHeaders:["Content-Type","Authorization"],
     })
